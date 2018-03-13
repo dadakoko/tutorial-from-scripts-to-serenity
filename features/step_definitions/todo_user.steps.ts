e89c5aca4080b58d648e376a3a0999179b25eedf
David Kony <david.kony@hesge.ch>
2018-03-13 10:43:58 +0100

    add code as the tutorial shows

--- features/step_definitions/todo_user.steps.ts
+++ features/step_definitions/todo_user.steps.ts
@@ -1,14 +1,31 @@
+import { Actor } from 'serenity-js/protractor';
+import {listOf} from "../../spec/text";
+import {Start} from "../../screenplay/tasks/start";
+import {AddATodoItem} from "../../screenplay/tasks/add_a_todo_item";
+import {protractor} from "protractor";
+import {BrowseTheWeb} from "serenity-js/lib/serenity-protractor";
+
 export = function todoUserSteps() {
 
     this.setDefaultTimeout(30 * 1000);  // The todomvc.com website can sometimes be a bit slow to load, so we tell
-                                        // Cucumber to give it up to 30 seconds to get ready.
 
-    this.Given(/^.*that (.*) has a todo list containing (.*)$/, function (name: string, items: string, callback) {
-        callback(null, 'pending');
+    let actor: Actor;                                    // Cucumber to give it up to 30 seconds to get ready.
+
+    this.Given(/^.*that (.*) has a todo list containing (.*)$/, function (name: string, items: string) {
+
+        actor = Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser));
+
+        actor.attemptsTo(
+            Start.withATodoListContaining(listOf(items))
+        );
+
+
     });
 
-    this.When(/^s?he adds (.*?) to (?:his|her) list$/, function (itemName: string, callback) {
-        callback(null, 'pending');
+    this.When(/^s?he adds (.*?) to (?:his|her) list$/, function (itemName: string) {
+        return actor.attemptsTo(
+            AddATodoItem.called(itemName)
+        )
     });
 
     this.Then(/^.* todo list should contain (.*?)$/, function (items: string, callback) {
---
+++ screenplay/tasks/start.ts
@@ -0,0 +1,25 @@
+import { PerformsTasks, Task } from 'serenity-js/protractor';
+import {AddATodoItem} from "./add_a_todo_item";
+import {Open} from "serenity-js/lib/serenity-protractor";
+
+export class Start implements Task {
+
+    static withATodoListContaining(items: string[]) {       // static method to improve the readability
+        return new Start(items);
+    }
+
+    performAs(actor: PerformsTasks): PromiseLike<void> {    // required by the Task interface
+        return actor.attemptsTo(
+            Open.browserOn('/examples/angularjs/'),// delegates the work to lower-level tasks
+            ...this.addAll(this.items)                          // ``...` is a spread operator,
+        );
+    }
+
+    constructor(private items: string[]) {                  // constructor assigning the list of items
+    }
+
+    private addAll(items: string[]): Task[] {                   // transforms a list of item names
+        return items.map(item => AddATodoItem.called(item));    // into a list of Tasks
+    }
+// to a private field
+}
